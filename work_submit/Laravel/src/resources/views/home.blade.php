4cf5e9f21701d1783367cd2f5ef95325ffcecb1c
M-Miyoshi-cc-20250512 <m-miyoshi@care-con.co.jp>
2025-11-21 18:28:02 +0900

    home.blade.php 11/21

--- work_submit/Laravel/src/resources/views/home.blade.php
+++ work_submit/Laravel/src/resources/views/home.blade.php
@@ -1,31 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TOP</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+
     <h1>Home page</h1>
-
-{{-- 検索バー --}}
-<form action="#" method="GET">
-    <input type="text" name="keyword" placeholder="Search for products">
-    <button type="submit">Search</button>
-</form>
-
-<hr>
 
 {{-- スライダー：新着商品 --}}
 <h2>New Products</h2>
-<div>
+<div class="slider">
+  <div class="slider-track">
     @foreach($newItems as $item)
-        <div>
-            <img src="{{ $item['image'] }}" width="100">
-            <p>{{ $item['name'] }}</p>
-            <p>￥{{ $item['price'] }}</p>
-        </div>
+      <div class="slide">
+        <img src="{{ $item['image'] }}" alt="{{ $item['name'] }}">
+        <p calss=products>{{ $item['name'] }}</p>
+        <p>￥{{ $item['price'] }}</p>
+      </div>
     @endforeach
+  </div>
+  <button class="prev">＜</button>
+  <button class="next">＞</button>
 </div>
 
 <hr>
@@ -44,14 +36,6 @@
 
 <hr>
 
-{{-- カテゴリ --}}
-<h2>Category</h2>
-<button>MEN</button>
-<button>WOMEN</button>
-<button>KIDS</button>
-
-<hr>
-
 {{-- ランキング --}}
 <h2>Trending products</h2>
 <ol>
@@ -62,5 +46,5 @@
         </li>
     @endforeach
 </ol>
-</body>
-</html>
+
+@endsection
