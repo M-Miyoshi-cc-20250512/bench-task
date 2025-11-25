cbcdd2b3f8b858c90fa8b5d3ca0b03df08fa8f46
M-Miyoshi-cc-20250512 <m-miyoshi@care-con.co.jp>
2025-11-25 18:48:57 +0900

    HomeController11/25

--- work_submit/Laravel/src/app/Http/Controllers/HomeController.php
+++ work_submit/Laravel/src/app/Http/Controllers/HomeController.php
@@ -21,14 +21,7 @@ class HomeController extends Controller
             ['id' => 5, 'name' => 'Sale productB', 'price' => 980,  'image' => '/dummy/s2.jpg'],
         ];
 
-        // ダミーのランキング商品
-        $rankingItems = [
-            ['id' => 6, 'name' => 'No1 product', 'price' => 2200, 'image' => '/dummy/r1.jpg'],
-            ['id' => 7, 'name' => 'No2 product', 'price' => 5400, 'image' => '/dummy/r2.jpg'],
-            ['id' => 8, 'name' => 'No3 product', 'price' => 3100, 'image' => '/dummy/r3.jpg'],
-        ];
-
-        return view('home', compact('newItems', 'saleItems', 'rankingItems'));
+        return view('home', compact('newItems', 'saleItems'));
     }
     public function search(Request $request)
     {
@@ -36,8 +29,14 @@ class HomeController extends Controller
 
         // ダミーデータ
         $allItems = [
-            ['name' => 'New productA', 'price' => 2000, 'image' => '/dummy/a.jpg'],
-            ['name' => 'Sale productB', 'price' => 980, 'image' => '/dummy/s2.jpg'],
+            ['name' => 'MEN Top A', 'price' => 2000, 'category' => 'men', 'size' => 'M', 'color' => 'Black', 'stock' => 10, 'image' => '/dummy/men1.jpg'],
+            ['name' => 'WOMEN Dress red', 'price' => 3000, 'category' => 'women', 'size' => 'L', 'color' => 'Red', 'stock' => 5, 'image' => '/dummy/women2.jpg'],
+            ['name' => 'WOMEN Dress black1', 'price' => 4000, 'category' => 'women', 'size' => 'S', 'color' => 'Black', 'stock' => 2, 'image' => '/dummy/women3.jpg'],
+            ['name' => 'WOMEN Dress black2', 'price' => 5000, 'category' => 'women', 'size' => 'M', 'color' => 'Black', 'stock' => 4, 'image' => '/dummy/women1.jpg'],
+            ['name' => 'KIDS Dress blue', 'price' => 5000, 'category' => 'kids', 'size' => '120', 'color' => 'Blue', 'stock' => 4, 'image' => '/dummy/kids5.jpg'],
+            ['name' => 'KIDS Dress white', 'price' => 6000, 'category' => 'kids', 'size' => '110', 'color' => 'White', 'stock' => 3, 'image' => '/dummy/kids4.jpg'],
+            ['name' => 'KIDS Dress black', 'price' => 2000, 'category' => 'kids', 'size' => '130', 'color' => 'Black', 'stock' => 6, 'image' => '/dummy/kids3.jpg'],
+            ['name' => 'KIDS Dress brown', 'price' => 2000, 'category' => 'kids', 'size' => '160', 'color' => 'Brown', 'stock' => 3, 'image' => '/dummy/kids1.jpg'],
         ];
 
         // 絞り込み
@@ -53,5 +52,30 @@ class HomeController extends Controller
             'saleItems' => [],
             'rankingItems' => [],
         ]);
+    }
+    public function categoryTrending($category)
+    {
+        // ダミーデータ：カテゴリごとのトレンド商品
+        $items = [
+            'men' => [
+                ['name' => 'MEN Trend 1', 'price' => 2000, 'image' => '/dummy/men1.jpg'],
+            ],
+            'women' => [
+                ['name' => 'WOMEN Trend 1', 'price' => 2200, 'image' => '/dummy/women1.jpg'],
+                ['name' => 'WOMEN Trend 2', 'price' => 3300, 'image' => '/dummy/women2.jpg'],
+            ],
+            'kids' => [
+                ['name' => 'KIDS Trend 1', 'price' => 1800, 'image' => '/dummy/kids1.jpg'],
+            ],
+        ];
+
+        // 存在するカテゴリかチェック
+        if (!array_key_exists($category, $items)) {
+            abort(404);
+        }
+
+        $trendingItems = $items[$category];
+
+        return view('category.trending', compact('trendingItems', 'category'));
     }
 }
